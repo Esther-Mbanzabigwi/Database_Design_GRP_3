226397d8b9cd9309bfb24cb5f7e231f7d3ea417c
Emmanue-Begati <e.yakubu@alustudent.com>
2024-11-08 19:53:17 +0200

    done

--- Task3/models.py
+++ Task3/models.py
@@ -3,20 +3,17 @@ from typing import Optional
 
 # Customer model
 class Customer(BaseModel):
-    #_id: str
     gender: str
     priorPurchases: int
 
 # Product model
 class Product(BaseModel):
-    #_id: str
     cost: float
     productImportance: str
     weightInGms: float
 
 # Order model
 class Order(BaseModel):
-    #_id: str
     customerId: str
     orderDate: str
     discountOffered: float
@@ -24,7 +21,6 @@ class Order(BaseModel):
 
 # Shipment model
 class Shipment(BaseModel):
-    #_id: str
     orderId: str
     warehouseBlock: str
     modeOfShipment: str
